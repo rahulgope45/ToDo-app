ea10994c2a6f8c474dd8e6bb6b13177dc3221b1c
rahulgope45 <rahuleile456@gmail.com>
2025-06-01 02:43:34 +0530

    Task can be added

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Todoform(){
+
+    const[inputvalue,envalue] = useState("")
+    const[handlevalue,fvalue] = useState("")
+
+    const handledinputvalue = (e) =>{
+        envalue(e.target.value)
+    }
+
+    const saveinput = () =>{
+        fvalue(inputvalue)
+    }
+
+
+
     return (
         <div className="Containerofbox">
-            <h1 className="headingofcontainer">Add Your Tasks </h1>
+            <div className="boxofcontainer">
+                <h1 className="headingofcontainer">Add Your Tasks </h1>
+            </div>
+            <div>
+                <input
+                type="text"
+                placeholder="Add Your Task"
+                value={inputvalue}
+                onChange={handledinputvalue}>
+                </input>
+                <button >
+                    {onclick = saveinput}
+                    Add
+                </button>
+            </div>
+            <div>
+                <p>{handlevalue}</p>
+            </div>
+            
 
         </div>
     )
