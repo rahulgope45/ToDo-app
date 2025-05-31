4b2bee1362d18364f04c84a81043e8609c5215ae
rahulgope45 <rahuleile456@gmail.com>
2025-05-31 16:23:18 +0530

    Todo app is starting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Todoform from "./components/Todoform";
+function App() {
+  return (
+    
+   <Todoform/>
+  );
+}
+
+
+
+
+export default App
---
+++ src/components/Todoform.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todoform(){
+    return (
+        <div className="Containerofbox">
+            <h1 className="headingofcontainer">Add Your Tasks </h1>
+
+        </div>
+    )
+}
+
+export default Todoform
+
