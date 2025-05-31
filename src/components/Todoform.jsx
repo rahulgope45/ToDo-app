7524fcec0edc4e57a9177147f9e6f059e70d4f7e
rahulgope45 <rahuleile456@gmail.com>
2025-06-01 03:54:44 +0530

    Now  we can add a task and delete a task css will be done further

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -4,15 +4,30 @@ import React, { useState } from "react";
 
 function Todoform(){
 
-    const[inputvalue,envalue] = useState("")
-    const[handlevalue,fvalue] = useState("")
+    const[inputvalue,envalue] = useState("") //for tracking input
+    const[handlevalue,fvalue] = useState([]) //for saving the input
+                                             //saving it in a array
+
+
 
     const handledinputvalue = (e) =>{
         envalue(e.target.value)
     }
 
     const saveinput = () =>{
-        fvalue(inputvalue)
+        if(inputvalue.trim() !== ""){
+            fvalue([...handlevalue,inputvalue])
+            envalue("")
+        }
+    }
+
+    //deleting the task one at a time
+
+    const inputDelete = (deletehandle) =>{
+
+        const deletelist = handlevalue.filter((_,index) => index !== deletehandle)
+        fvalue(deletelist)
+        
     }
 
 
@@ -29,14 +44,26 @@ function Todoform(){
                 value={inputvalue}
                 onChange={handledinputvalue}>
                 </input>
-                <button >
-                    {onclick = saveinput}
+                <button onClick = {saveinput}>
+                    
                     Add
                 </button>
             </div>
             <div>
+                {handlevalue.map((item,index)=>(
+                    
+                    <p key={index}>*{item}
+                    <button onClick={() => inputDelete(index)}> 
+                        
+                        ❌
+                        </button>
+                        <input type="radio"></input>
+                    </p>
+                    
+                ))}
                 
-                <p>{handlevalue}</p>
+                
+               
             </div>
             
 
