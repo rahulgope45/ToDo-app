a518149ea2120549aa2c1406c372d6ced13614fe
rahulgope45 <rahuleile456@gmail.com>
2025-06-11 01:45:18 +0530

    90% it is done

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -5,14 +5,13 @@ function Todoapp(){
 
     //setting up table
 
-    const [activestate,setState] = useState('')
+    const [activestate,setState] = useState('Tasks')
     const renderTable = ()=>{
-        
-    }
-
 
     const [input,setInput] = useState("")
     const [striked,setStriked] =useState([])
+    //button color
+    const [clicked,setClicked] = useState(false)
     function HandleInput(){
         // const newInput = document.getElementById("Tasks").value
         // document.getElementById("Tasks").value = "" //
@@ -42,22 +41,14 @@ function Todoapp(){
 
     }
 
+    const filterratedtasks = ()=>{
 
-    // using switch case here to make tabs
+    }
 
-    
-
-
-   
-
-
-
-    return(
-        <>
-        <div className="Container-app">
-            <div className="heading"> 
-                <h1>Taskify</h1>
-            </div>
+    switch(activestate){
+        case 'Tasks':
+            return <div className="Container-app">
+            
             <div className="Task-container">
                 <input type="text" value={input}
                     onChange={(e) => setInput(e.target.value)}
@@ -90,6 +81,64 @@ function Todoapp(){
             </div>
 
         </div>
+
+        case 'Active-Tasks':
+            return <div>
+                <h2>Active Tasks</h2>
+                <ol>
+                    {striked.map((items,index)=>(
+                        <li key={index}>
+                            {items.text}
+                        </li>
+                    ))}
+                </ol>
+            </div>
+
+        case 'Completed-Tasks':
+            return <div>
+                <h2>Completed Tasks</h2>
+                 <ol>
+                    {striked
+                .filter(items => items.completed)  // only completed tasks
+                .map((items, index) => (
+                    <li key={index}>{items.text}</li>
+                ))
+            }
+
+                </ol>
+            </div>
+
+        case 'Journal':
+            return <div>
+                <h2>Journal</h2>
+            </div>
+
+        default: 
+        return null
+        
+    }
+
+
+    }
+
+
+
+    return(
+        <><div className="heading"> 
+                <h1>Taskify</h1>
+            </div>
+         <div style={{ display: 'flex', gap: '1rem', marginBottom: '1rem' }}>
+            <button onClick={() => setState('Tasks')}
+            
+            >Tasks</button>
+            <button onClick={() => setState('Active-Tasks')}>Active-Tasks</button>
+            <button onClick={() => setState('Completed-Tasks')}>Completed-Tasks</button>
+            <button onClick={() => setState('Journal')}>Journal</button>
+        </div>
+        <div style={{ padding: '1rem', border: '1px solid #ccc' }}>
+            {renderTable()}
+        </div>
+        
         
         </>
     )
